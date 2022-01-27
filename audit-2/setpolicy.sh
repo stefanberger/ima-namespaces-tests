445903cfbf8efd7a54948f13035ad29e5828a0af
Stefan Berger <stefanb@linux.ibm.com>
2022-01-27 09:32:48 -0500

    Add test case for testing mapping of uid and gid values
    Add a test case for testing the mapping of uid and gid values.
    
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

---
+++ audit-2/setpolicy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+#set -x
+
+SYNCFILE=${SYNCFILE:-syncfile}  #make shellcheck happy
+
+. ./ns-common.sh
+
+mnt_securityfs /mnt
+
+policy="audit func=BPRM_CHECK uid=0 gid=0"
+echo "${policy}" > /mnt/ima/policy || {
+  echo " Error: Could not set audit policy."
+  echo > "${FAILFILE}"
+}
+
+echo > "${SYNCFILE}"
+
+sleep 3
