5b5075886a4d7eb39dbda389bd46b4fc72555fda
Stefan Berger <stefanb@linux.ibm.com>
2022-02-01 21:43:47 -0500

    Test SETXATTR_CHECK with varying selection of allowed hash algos
    Add a test case that creates an appraisal policy rule with
    func=SETXATTR_CHECK and a varying set of sha algorithms. Try out signing
    a testfile with hashed with varying types of sha hashes of which those
    that are in the policy rule must succeed and others must fail.
    
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

---
+++ appraise-6/setxattr-check.sh
@@ -0,0 +1,80 @@
+#!/bin/env sh
+
+# SPDX-License-Identifier: BSD-2-Clause
+
+# shellcheck disable=SC2059,SC3043,SC3057
+# set -x
+
+get_hash_algo_strings()
+{
+  local flags="${1}"
+
+  local algos=""
+
+  [ $((flags & 1)) -ne 0 ] && algos="${algos},sha256"
+  [ $((flags & 2)) -ne 0 ] && algos="${algos},sha384"
+  [ $((flags & 4)) -ne 0 ] && algos="${algos},sha512"
+
+  # remove leading ,
+  [ "${algos:0:1}" = "," ] && algos="${algos:1}"
+  echo "${algos}"
+}
+
+. ./ns-common.sh
+
+mnt_securityfs "/mnt"
+
+ALGOS=${ALGOS:-1}
+KEY=./rsakey.pem
+
+algos_str=$(get_hash_algo_strings "${ALGOS}")
+[ -z "${algos_str}" ] && exit "${SUCCESS:-0}"
+
+policy="appraise func=SETXATTR_CHECK appraise_algos=${algos_str} \n"
+
+printf "${policy}" > /mnt/ima/policy || {
+  echo " Error: Could not set appraise policy with SETXATTR_CHECK rule. Does IMA-ns support IMA-appraise?"
+  echo " policy: |${policy}|"
+  exit "${SKIP:-3}"
+}
+
+echo > testfile
+algo=1
+while [ "${algo}" -le 4 ]; do
+  pass=0
+  algo_str=$(get_hash_algo_strings "${algo}")
+
+  evmctl ima_sign --imasig --key "${KEY}" -a "${algo_str}" testfile 1>/dev/null 2>&1 && pass=1
+
+  # if algo is a bit set in ALGOS, then the test must pass, since algo is in policy
+  mustpass=$((ALGOS & algo))
+  case "${pass}" in
+  0)
+    case "${mustpass}" in
+    0)
+      # echo " GOOD: Could not sign file with hash ${algo_str} since not in ${algos_str}"
+      ;;
+    *)
+      echo " Error: Could not write xattr for file signed with hash ${algo_str} even though it should be possible"
+      echo " policy: |${policy}|"
+      exit "${FAIL:-1}"
+      ;;
+    esac
+    ;;
+  1)
+    case "${mustpass}" in
+    0)
+      echo " Error: Could write xattr for file signed with hash ${algo_str} even though it should NOT be possible"
+      echo " policy: |${policy}|"
+      exit "${FAIL:-1}"
+      ;;
+    *)
+      # echo " GOOD: Could sign file with hash ${algo_str} since it is in ${algos_str}"
+      ;;
+    esac
+    ;;
+  esac
+  algo=$((algo * 2))
+done
+
+exit "${SUCCESS:-0}"
