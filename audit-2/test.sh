4a8c156168194dfac5548ed60a7ee1b149790bc5
Stefan Berger <stefanb@linux.ibm.com>
2022-02-03 07:02:03 -0500

    Add test for testing that non-root users cannot set audit policies
    Add a test case that tests that non-root users cannot set audit
    policy rules.
    
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

---
+++ audit-2/test.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+# SPDX-License-Identifier: BSD-2-Clause
+
+# shellcheck disable=SC2009
+
+#set -x
+
+DIR="$(dirname "$0")"
+ROOT="${DIR}/.."
+
+source "${ROOT}/common.sh"
+
+check_ima_support
+
+if [ "$(id -u)" -eq 0 ]; then
+  test_users="ftp apache tss nobody"
+  found=0
+  for TEST_USER in ${test_users}; do
+    if id -u "${TEST_USER}" &>/dev/null && \
+       id -g "${TEST_USER}" &>/dev/null; then
+      found=1
+      break
+    fi
+  done
+  if [ "${found}" -eq 0 ]; then
+    echo " Error: Could not find a suitable user to run test with"
+    exit "${SKIP:-3}"
+  fi
+else
+  TEST_USER="$(id -un)"
+fi
+
+setup_busybox_container \
+	"${ROOT}/ns-common.sh" \
+	"${DIR}/fail-setpolicy.sh"
+
+rootfs=$(get_busybox_container_root)
+
+echo "INFO: Testing that non-root user '${TEST_USER}' cannot set audit policy rules"
+
+chown -R "${TEST_USER}:${TEST_USER}" "${rootfs}"
+
+sudo -u "${TEST_USER}" \
+  env PATH=/bin:/usr/bin \
+  unshare --user --map-root-user --mount-proc \
+    --pid --fork --root "${rootfs}" bin/sh ./fail-setpolicy.sh
+rc=$?
+if [ "${rc}" -ne 0 ]; then
+  echo " Error: Test failed in IMA namespace"
+  exit "${rc}"
+fi
+
+echo "INFO: Pass"
+
+exit "${SUCCESS:-0}"
