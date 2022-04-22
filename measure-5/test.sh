346f56fe87a201fee31533b45f9a4eeeb52ac3f9
Stefan Berger <stefanb@linux.ibm.com>
2022-04-22 09:12:56 -0400

    Add test case for measurement policy using different templates
    This test case that uses different templates for logging of BPRM_CHECK
    and MMAP_CHECK.
    
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

---
+++ measure-5/test.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# SPDX-License-Identifier: BSD-2-Clause
+
+# shellcheck disable=SC1091
+#set -x
+
+DIR="$(dirname "$0")"
+ROOT="${DIR}/.."
+
+source "${ROOT}/common.sh"
+
+check_ima_support
+
+setup_busybox_container \
+	"${ROOT}/ns-common.sh" \
+	"${ROOT}/check.sh" \
+	"${DIR}/measure.sh"
+
+if ! check_ns_measure_support; then
+  echo " Error: IMA-ns does not support IMA-measurement"
+  exit "${SKIP:-3}"
+fi
+
+copy_elf_busybox_container "$(type -P evmctl)"
+
+# Test meassurements caused by executables and libraries run in namespace
+
+echo "INFO: Testing measuring (BPRM_CHECK + MMAP_CHECK) with different templates inside container"
+
+run_busybox_container ./measure.sh
+rc=$?
+if [ $rc -ne 0 ] ; then
+  echo " Error: Test failed in IMA namespace."
+  exit "$rc"
+fi
+
+echo "INFO: Pass test 1"
+
+exit "${SUCCESS:-0}"
